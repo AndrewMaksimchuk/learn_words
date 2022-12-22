f7d73254212417d26d147e7da272144f5f4b742e
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2022-12-22 15:36:55 +0200

    Init

---
+++ notify.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env gjs
+
+const GLib = imports.gi.GLib;
+const Notify = imports.gi.Notify;
+
+const [ok, contents] = GLib.file_get_contents("dictionary.json");
+
+if (ok) {
+    const utf8decoder = new TextDecoder();
+    const str = utf8decoder.decode(contents)
+    const map = JSON.parse(str);
+
+    const dictionaryLength = map.length;
+    const wordIndex = Math.floor(Math.random() * dictionaryLength);
+    const word = map[wordIndex];
+
+    Notify.init("learn");
+
+    const notify = {
+        summary: word.summary,
+        body: word.body,
+        "icon-name": "dialog-information"
+    }
+
+    const learn = new Notify.Notification(notify);
+    learn.show();
+}
+
+
