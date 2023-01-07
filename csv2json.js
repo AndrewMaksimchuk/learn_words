f44764d029aef775d6713870b127615d82fc5f87
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-01-07 16:12:55 +0200

    Added: Check for exist file

--- csv2json.js
+++ csv2json.js
@@ -1,10 +1,12 @@
-const { readFileSync } = require("fs");
+const { readFileSync, existsSync } = require("fs");
+const { join } = require("path");
 
 const maped = (value) => {
     const [summary, ...body] = value.split(" ");
     return { summary, body: body.toString().replaceAll(",", ", ") };
 }
 
-const csv2json = (file = "words") => readFileSync(file).toString().split("\n").map(maped);
+const csv2json = (file = "words") => existsSync(join(__dirname, file))
+    && readFileSync(file).toString().split("\n").map(maped);
 
 module.exports = { csv2json }
