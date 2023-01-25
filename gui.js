249087ef9c42e5429f3537d110cc7e31f87f194e
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-01-25 15:27:40 +0200

    Added: Single word update in gui (#6)

--- gui.js
+++ gui.js
@@ -15,6 +15,10 @@ const entry = new Gtk.Entry({
     buffer: new Gtk.EntryBuffer()
 });
 
+const entryUpdate = new Gtk.Entry({
+    buffer: new Gtk.EntryBuffer()
+});
+
 const statusBar = new Gtk.Statusbar();
 
 const button = new Gtk.Button({ label: 'Add to dictionary!' });
@@ -26,12 +30,26 @@ button.connect('clicked', () => {
     
     statusBar.push(0, `У словник додано: ${inputText}`);
     entry.set_text("");
-    win.set_focus(entry)
+    win.set_focus(entry);
 });
 
-box.add(entry)
-box.add(button)
-box.add(statusBar)
+const buttonUpdate = new Gtk.Button({ label: 'Update word!' });
+buttonUpdate.connect('clicked', () => {
+    const inputText = entryUpdate.get_buffer().text;
+    if (Boolean(inputText) === false) return;
+
+    outputStream.write(`\n${inputText}`, null);
+    
+    statusBar.push(0, `Оновлено слово: ${inputText}`);
+    entryUpdate.set_text("");
+    win.set_focus(entryUpdate);
+});
+
+box.add(entry);
+box.add(button);
+box.add(entryUpdate);
+box.add(buttonUpdate);
+box.add(statusBar);
 
 const win = new Gtk.Window({ defaultWidth: 500 });
 win.set_title("Learn words | Add new word");
