ab933ad0c96e59b97dd3ab825fc5f27753c2b18a
Andrew Maksimchuk <amaksimcthuk@gmail.com>
2025-06-07 11:47:12 +0300

    [cli] clear words file #23

--- update_dictionary.js
+++ update_dictionary.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const { existsSync } = require("fs");
+const { existsSync, copyFileSync, unlinkSync, writeFileSync } = require("fs");
+const { join } = require("path");
 const { csv2json } = require("./csv2json");
 const { add2json } = require("./add2json");
 const { save2json } = require("./save2json");
 const { obj2string } = require("./obj2string");
-const { join } = require("path");
 
 const errMsg = `Nothing to add.
 Check if "words" file exist and is not empty.
@@ -15,7 +15,19 @@ const printUpdates = (csv = []) => {
   process.stdout.write(`[ INFO ] Dictionary update with ${csv.length} words\n`);
 }
 
-if(!existsSync(join(__dirname, "words"))) {
+const rawWordsFile = join(__dirname, "words")
+
+const save2history = () => {
+  const dest = join(__dirname, "history", String(Date.now()));
+  copyFileSync(rawWordsFile, dest);
+}
+
+const clearWordsFile = () => {
+  unlinkSync(rawWordsFile);
+  writeFileSync(rawWordsFile, "");
+}
+
+if(!existsSync(rawWordsFile)) {
   process.stdout.write(errMsg);
 }
 
@@ -24,5 +36,5 @@ const json = csv && add2json(csv);
 const data = json && obj2string(json);
 
 data 
-    ? (save2json(data), printUpdates(csv))
+    ? (save2history(), save2json(data), printUpdates(csv), clearWordsFile())
     : process.stdout.write(errMsg);
