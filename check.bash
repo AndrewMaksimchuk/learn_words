748e01f3f7bc920f2c9eade55e56c45af885b50a
Andrew Maksimchuk <amaksimcthuk@gmail.com>
2025-06-21 17:39:24 +0300

    [cli] check - add word id missed #25

--- check.bash
+++ check.bash
@@ -2,6 +2,18 @@
 
 dir=$(dirname $0)
 
+function learnwords_add_word_and_open() {
+ed -s $dir/words << EOF
+0a
+$1
+.
+w
+q
+EOF
+
+vi +startinsert -c ":normal A  " $dir/words 
+}
+
 if [[ -z $1 ]]; then
     echo "Add word for search"
     exit
@@ -10,7 +22,18 @@ fi
 words=$(jq '.[] | "\(.summary): \(.body)"' "$dir/dictionary.json" | grep "$1" | sort | tr -d '"')
 
 if [[ -z $words ]]; then
-    echo "Your word \"$1\" not contain in dictionary"
+    echo "\"$1\" not contain in dictionary"
+    read -p "Want to add this word? (y/n): " answer
+
+    if [[ -z "$answer" ]]; then
+      exit
+    fi
+
+    if [[ "$answer" = 'n' ]]; then
+      exit
+    fi
+
+    learnwords_add_word_and_open "$1"
     exit
 fi
 
